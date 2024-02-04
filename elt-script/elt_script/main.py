24b51f060950a24568d9abba64292a33e4c75ecb
Edward Heaver <edward.heaver01@gmail.com>
2024-02-04 16:54:22 +0000

    got generic sql client in script image and vol mounts to make life simpler

--- elt-script/elt_script/main.py
+++ elt-script/elt_script/main.py
@@ -1,6 +1,8 @@
 import os
+from subprocess import call
 
 
 if __name__ == "__main__":
     print("Hello world")
     print(f"db name: {os.environ['SOURCE_DB']!r}")
+    os.system("ls -lah /usr/local/bin")
